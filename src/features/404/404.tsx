21cf475208c37b331a42a04066d21389b94aff4f
Stefan Djordjevic <stefan.djordjevic@sbgenomics.com>
2021-11-06 18:00:05 +0100

    feat: add 404 page

---
+++ src/features/404/404.tsx
@@ -0,0 +1,11 @@
+
+const NoMatch = () => {
+    return (
+        <div className={"404-page"}>
+            Ooops... <br/>
+            Page cannot be found.
+        </div>
+    )
+};
+
+export default NoMatch;
